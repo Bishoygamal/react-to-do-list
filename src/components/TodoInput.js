9f33d56a68e85dc5969d55fe014836993398dfa8
Beshoy Gamal <Beshoy.Gamal@itworx.com>
2020-01-14 13:49:07 +0200

    first commit

---
+++ src/components/TodoInput.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class TodoInput extends Component {
+    render() {
+        return (
+            <h2>
+                hello from to do input
+            </h2>
+        )
+    }
+}
